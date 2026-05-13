0da1ae16e4ff7308aed8dab96f6a8262695d66c7
hash-m <hashim.khan04@icloud.com>
2026-05-13 15:04:46 +0100

    initial setup

---
+++ quickcode/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'QuickCode',
+      theme: ThemeData(
+        colorScheme: .fromSeed(seedColor: Colors.black),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            
+        );
+    }
+}
